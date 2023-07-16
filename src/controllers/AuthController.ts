a424729c850f1da6ed3356b76b47fe378cf4fcf7
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-16 21:23:24 +0700

    Password Hashing (Bcrypt)

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
+import PasswordHash from "../utils/PasswordHash";
 const db = require("../db/models");
+
 class AuthController {
-  //Show all data
+  //Register
   register = async (req: Request, res: Response): Promise<Response> => {
     let { username, password } = req.body;
+    const hashedPassword = await PasswordHash.hash(password);
 
-    const createdUser = await db.user.create({ username, password });
+    await db.user.create({ username, password: hashedPassword });
     return res.send("Registration Success");
   };
-
-  //Create one data
+  //Login
   login(req: Request, res: Response): Response {
     return res.send("Register");
   }
