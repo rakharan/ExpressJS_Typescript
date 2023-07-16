13ad43e320b9033c060b1c77794397cdca41f195
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-16 18:52:05 +0700

    First Commit
    Initialize express using class

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Response, Request, NextFunction, Application } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+import cors from "cors";
+
+//Routes
+import UserRoutes from "./routes/UserRoutes";
+
+class App {
+  public app: Application;
+  constructor() {
+    this.app = express();
+    this.plugins();
+    this.routes();
+  }
+
+  protected plugins(): void {
+    this.app.use(express.json());
+    this.app.use(morgan("dev"));
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
+
+  protected routes(): void {
+    this.app.route("/").get((req: Request, res: Response): void => {
+      res.send("Hello world");
+    });
+
+    this.app.use("/users", UserRoutes);
+  }
+}
+
+const port: number = 3000;
+const app = new App().app;
+app.listen(port, () => {
+  console.log("App is running on " + port);
+});
