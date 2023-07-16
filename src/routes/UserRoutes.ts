82adc6bf26dd4a64954983bbb7dd1eff20efdc5a
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-16 19:01:15 +0700

    Routes Initialization

---
+++ src/routes/UserRoutes.ts
@@ -0,0 +1,21 @@
+import { Router, Request, Response } from "express";
+import IRouter from "./RouteInterface";
+
+//Controller
+import UserController from "../controllers/UserController";
+
+class UserRoutes implements IRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+  public routes(): void {
+    this.router.get("/", UserController.index);
+
+    this.router.post("/", UserController.create);
+  }
+}
+
+export default new UserRoutes().router;
