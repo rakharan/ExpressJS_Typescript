b71e426c203fae7b190abee092bffcdd87670194
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-18 23:13:14 +0700

    [Refactor]:Refactoring AuthMiddleware.ts

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -2,23 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 export const auth = (req: Request, res: Response, next: NextFunction): any => {
-  if (!req.headers.authorization) {
-    return res.status(401).send("Unauthorized")
+  const authorizationHeader = req.headers.authorization;
+  const secretKey = process.env.JWT_SECRET_KEY || "secret";
+
+  if (!authorizationHeader) {
+    return res.status(401).send("Unauthorized");
   }
 
-  let secretKey = process.env.JWT_SECRET_KEY || "secret"
-  const token: string = req.headers.authorization.split(" ")[1]
+  const token = authorizationHeader.split(" ")[1];
 
   try {
-    const credential: string | object = jwt.verify(token, secretKey)
+    const credential = jwt.verify(token, secretKey);
+
     if (credential) {
-      req.app.locals.credential = credential
-      next()
+      req.app.locals.credential = credential;
+      next();
     } else {
-      return res.send("token invalid")
+      return res.send("Token invalid");
     }
-  }
-  catch (error) {
-    return res.send(error)
+  } catch (error) {
+    return res.send(error);
   }
 };
+
