8c3623b31f6fe0618d6ff1667e5a878e06bfae85
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-18 16:16:10 +0700

    [Refactor]:Auth Controller
    Refactoring Auth Controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,20 +1,37 @@
 import { Request, Response } from "express";
-import PasswordHash from "../utils/PasswordHash";
+import Authentication from "../utils/Authentication";
+import { compare } from "bcrypt";
 const db = require("../db/models");
 
 class AuthController {
   //Register
   register = async (req: Request, res: Response): Promise<Response> => {
     let { username, password } = req.body;
-    const hashedPassword = await PasswordHash.hash(password);
+    const hashedPassword = await Authentication.passwordHash(password);
 
     await db.user.create({ username, password: hashedPassword });
     return res.send("Registration Success");
   };
   //Login
-  login(req: Request, res: Response): Response {
-    return res.send("Register");
+  login = async (req: Request, res: Response): Promise<Response> => {
+
+    //cari data user by username
+    let { username, password } = req.body
+    const user = await db.user.findOne({
+      where: { username }
+    })
+
+    //check password
+    let compare = await Authentication.passwordCompare(password, user.password)
+
+    // generate token
+    if (compare) {
+      let token = Authentication.generateToken(user.id, username, user.password)
+      return res.send({
+        token
+      });
+    }
+    return res.send("Authentication failed")
   }
 }
-
 export default new AuthController();
