5fb2c91765caceac50f1f5fbb4eae267ceec5c39
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-18 21:41:25 +0700

    [Feat]:Profile Endpoint

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -2,11 +2,13 @@ import BaseRoutes from "./BaseRouter";
 //Controller
 import AuthController from "../controllers/AuthController";
 import validate from "../middleware/AuthValidator";
+import { auth } from "../middleware/AuthMiddleware"
 
 class AuthRoutes extends BaseRoutes {
   public routes(): void {
     this.router.post("/register", validate, AuthController.register);
     this.router.post("/login", validate, AuthController.login);
+    this.router.get("/profile", auth, AuthController.profile)
   }
 }
 
